5bc83fb457ecb25e0f26429d1270ec548eec93f3
rakshith-gannarapu <z1862017@students.niu.edu>
2020-03-05 23:10:45 -0600

    First commit

---
+++ Lab4/src/application/HouseAreaFactory.java
@@ -0,0 +1,26 @@
+package application;
+
+public class HouseAreaFactory extends HouseFactory{
+
+	@Override
+	public HouseEntity createItem(String item) {
+		if(item.equals("House")) {
+			return new HouseArea("House");	
+		}
+		else if(item.equals("Kitchen")) {
+			return new HouseArea("Kitchen");	
+		}
+		else if(item.equals("Hall")) {
+			return new HouseArea("Hall");	
+		}
+		else if(item.equals("BedBath")) {
+			return new HouseArea("BedBath");	
+		}
+		else {
+			return null;
+		}
+		
+	}
+
+	
+}
---
+++ Lab4/src/application/HouseFactory.java
@@ -0,0 +1,9 @@
+package application;
+
+public abstract class HouseFactory {
+	public abstract HouseEntity createItem(String item) ;
+	public HouseEntity buildHouse(String type) {
+		HouseEntity houseentity = createItem(type);
+		return houseentity;
+	}
+}
